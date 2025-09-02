499a04858bc715b135dfe8e19c921e46df2c4a9b
khushboormeshram <khushboomeshram6@gmail.com>
2025-09-03 01:25:56 +0530

    Update

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # <- This allows requests from your Flutter web app
+
 
 app = Flask(__name__)
 
